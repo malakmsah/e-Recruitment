f3bbeef5f70aef4581b918a8726a1e0acb1a6147
Malak <malak.abuhammad@gmail.com>
2019-05-20 00:15:23 +0300

    Update ConnectionDao.java

--- code/src/java/daos/ConnectionDao.java
+++ code/src/java/daos/ConnectionDao.java
@@ -27,9 +27,14 @@ public class ConnectionDao implements Serializable {
 
         if (!useConnectionPool) {
             //oracleUrl = "jdbc:oracle:thin:@52.232.34.123:1521:CE471DB";
-            oracleUrl = "jdbc:oracle:thin:@192.168.120.213:1521:xe";
-            databaseUsername = "STD_SRVCS";
-            databasePassword = "CE471_second_2017";
+            //oracleUrl = "jdbc:oracle:thin:@192.168.120.213:1521:xe";
+            //databaseUsername = "STD_SRVCS";
+            //databasePassword = "CE471_second_2017";
+            
+            // Online DB
+            oracleUrl = "gjobs.ck0xb2jwzofh.us-west-2.rds.amazonaws.com";
+            databaseUsername = "gjobs";
+            databasePassword = "gjobs12345";
         }
 
         FacesContext context = FacesContext.getCurrentInstance();
